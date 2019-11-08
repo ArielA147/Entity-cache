7b2f154802a1c92ac0eaa6dc09d0eb72fae662d4
Ariel Alexi <ariel.147@gmail.com>
2019-11-08 16:27:32 +0200

    creating and update the Repository Provider file

--- src/RepositoryProvider.java
+++ src/RepositoryProvider.java
@@ -1,2 +1,50 @@
-public class RepositoryProvider {
+import com.google.gson.Gson;
+
+import java.util.List;
+
+public class RepositoryProvider implements IProvider {
+
+    private List<Gson> data;
+    private String jsonInString = "{\"id\":\"1\"}";
+
+    public Gson getData(int id) {
+        MyEntity entity;
+        for (Gson curGson : data) {
+            entity = curGson.fromJson(jsonInString, MyEntity.class);
+            if (entity.getId() == id) {
+                return curGson;
+            }
+        }
+        return null;
+    }
+
+    public List<Gson> getAll() {
+        return data;
+    }
+
+    public void add(Gson entity) throws Exception {
+        this.data.add(entity);
+    }
+
+    public void update(Gson entity) throws Exception {
+        int entityId = entity.fromJson(jsonInString, MyEntity.class).getId();
+        for (int i = 0; i < this.data.size(); i++) {
+            // finding the entity we are updating
+            if (entityId == data.get(i).fromJson(jsonInString, MyEntity.class).getId()) {
+                data.add(i, entity);
+                break;
+            }
+        }
+    }
+
+    public void remove(Gson entity) throws Exception {
+        int entityId = entity.fromJson(jsonInString, MyEntity.class).getId();
+        for (int i = 0; i < this.data.size(); i++) {
+            // finding the entity we are updating
+            if (entityId == data.get(i).fromJson(jsonInString, MyEntity.class).getId()) {
+                data.remove(i);
+                break;
+            }
+        }
+    }
 }
