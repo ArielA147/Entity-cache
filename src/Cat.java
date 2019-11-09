7ef3de2dd983470258031bdd8fefe7188c4e405f
Ariel Alexi <ariel.147@gmail.com>
2019-11-09 23:42:23 +0200

    creating an entity class of cat for testing

--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class Cat implements IEntity {
 
-public class Cat {
+    private int id;
+    private String name;
+
+    public Cat(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public void setId(int number) {
+        this.id = number;
+    }
 }
